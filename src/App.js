0c6a31ca97b8d3bf15ef103cb3044a6630c6846f
Matias Correa <frenzy9304@gmail.com>
2022-11-21 20:22:44 -0300

    Creacion del Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import './components/Navbar/Navbar.css'
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = ()=>{
+
+    return (
+        <div className="barraNavegacion">
+            <h1 className="barraTitulo"> Artnology</h1>
+            <div className="barraOpciones">
+                <button className="opciones"> Inicio </button>
+                <button className="opciones"> Articulos </button>
+                <button className="opciones"> Ingreso </button>
+            </div>
+        </div>
+        
+    )
+}
+export default Navbar;
