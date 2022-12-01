4c4c29b17538ceddd5f3fe43a22b6e1cdd1082d1
Matias Correa <frenzy9304@gmail.com>
2022-12-01 20:19:29 -0300

    incorporacion de ItemCounter

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,14 +1,14 @@
 import Count from "../Count/Count"
 const ItemCount = ()=>{
 
-    const handleOnAdd = (quantity)=>{
+    const ManejaOnAdd = (quantity)=>{
         console.log("Se agrego al carrito " + quantity)
     }
     return(
 
         <div className="ContainerCounter">
             <p> nombre del producto</p>
-            <Count initial={0} stock={15} onAdd={handleOnAdd}/>
+            <Count initial={0} stock={15} onAdd={ManejaOnAdd}/>
                         
         </div>
     )
