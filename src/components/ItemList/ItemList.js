316be5ad8d5ee73273dda1d7829f2ce33a515705
Matias Correa <frenzy9304@gmail.com>
2022-12-01 21:22:18 -0300

    Solucion error de carpeta

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,13 +1,9 @@
 import ItemCount from "../ItemCount/ItemCount"
-import Items from "../Items/Items"
+
 const ItemList = ()=>{
-
     return(
-
-        <div className="ContainerProduc">
-            {/* aca va lo relacionado al producto*/}
-            <Items/>
-            <ItemCount initial={0} stock={15} />
+        <div className='ContainerProduc'> 
+            <ItemCount/>
         </div>
     )
 }
